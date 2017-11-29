14c3538085ec6258e4962fb3496148f8d0ee68d5
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-11-28 23:29:56 -0800

    Add famicom-pro-action-rocky encoding support

--- cli.js
+++ cli.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 'use strict';
 
-const { encode, decode, encodeHex, decodeHex } = require('nes-game-genie');
+const { encodeGG, decodeGG, encodeHex, decodeHex } = require('nes-game-genie');
+const { encodeRocky, decodeRocky } = require('famicom-pro-action-rocky');
 
 if (process.argv.length < 3) {
   process.stderr.write(`NES Game Genie encoder/decoder
@@ -13,21 +14,23 @@ code: a Game Genie code or hex code
 }
 
 const code = process.argv.slice(2)[0];
-process.stdout.write(`Input:    ${code}\n\n`);
+process.stdout.write(`Input:       ${code}\n\n`);
 
-const decoded = decode(code);
+const decoded = decodeGG(code);
 //console.log(decoded);
-process.stdout.write(`Address:  ${decoded.address.toString(16)}\n`);
+process.stdout.write(`Address:     ${decoded.address.toString(16)}\n`);
 if (decoded.key !== undefined) {
-  process.stdout.write(`Key:      ${decoded.key.toString(16)}\n`);
+  process.stdout.write(`Key:         ${decoded.key.toString(16)}\n`);
 }
-process.stdout.write(`Wantskey  ${decoded.wantskey}\n`);
-process.stdout.write(`Value:    ${decoded.value.toString(16)}\n`);
+process.stdout.write(`Wantskey:    ${decoded.wantskey}\n`);
+process.stdout.write(`Value:       ${decoded.value.toString(16)}\n`);
 process.stdout.write(`\n`);
 
 const hexCode = encodeHex(decoded.address, decoded.value, decoded.key, decoded.wantskey);
-process.stdout.write(`Hex code: ${hexCode}\n`);
+process.stdout.write(`Hex code:    ${hexCode}\n`);
 
-const ggCode = encode(decoded.address, decoded.value, decoded.key, decoded.wantskey);
-process.stdout.write(`GG code:  ${ggCode}\n`);
+const frCode = encodeRocky(decoded.address, decoded.value, decoded.key);
+process.stdout.write(`Rocky code:  ${frCode}\n`);
 
+const ggCode = encodeGG(decoded.address, decoded.value, decoded.key, decoded.wantskey);
+process.stdout.write(`Game Genie:  ${ggCode}\n`);
